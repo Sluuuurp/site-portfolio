3a118d654829a5077da680c3c39ab247f6f809c7
Florian ALEXANDRE <florian.alexandre17@gmail.com>
2025-04-02 10:25:04 +0200

    Ajout de la section Parcours et du fichier js

---
+++ main.js
@@ -0,0 +1,54 @@
+// gestion de l'anim des tuiles
+
+const tuiles = document.querySelectorAll(".parcours__tuile")
+
+tuiles.forEach(tuile =>{
+    tuile.addEventListener('mouseover', (e) =>{
+        const tuilePlacement = tuile.getBoundingClientRect(); //recup poisition de la tuile
+        const tuileCentreX = tuilePlacement.left + tuilePlacement.width / 2;
+        const tuileCentreY = tuilePlacement.top + tuilePlacement.height / 2;
+
+        const mouseX = e.clientX; //position de la souris
+        const mouseY = e.clientY;
+
+        const distCentreX = (mouseX - tuileCentreX) / (tuilePlacement.width / 2)
+        const distCentreY = (mouseY - tuileCentreY) / (tuilePlacement.height / 2)
+
+        tuile.style.transform = `rotateX(${distCentreY *30}deg) rotateY(${distCentreX *30}deg)`
+    })
+
+    tuiles.forEach(tuile => {
+        tuile.addEventListener('mouseleave', () => {
+            tuile.style.transform = 'rotateX(0deg) rotateY(0deg)'
+        })
+    })     
+})
+
+//gestion du tri
+//recup bouton
+const boutons = document.querySelectorAll(".bouton-tri")
+//gestion de la selection des boutons
+boutons.forEach(bouton=>{
+    bouton.addEventListener('click', ()=> {
+        boutons.forEach(bouton => {bouton.classList.remove("selectionner")}) //deselectionne
+        bouton.classList.add("selectionner") //selectiionne
+        
+        //affichage des tuiles
+        tuiles.forEach(tuile => {
+            tuile.classList.remove('hidden')//remise a zero
+            if (bouton.textContent === "TOUT"){
+                tuile.classList.remove('hidden')
+            }
+            else if (tuile.classList[1] !== bouton.textContent){
+                tuile.classList.add('hidden')
+            }
+        })
+    })
+})
+
+
+
+
+
+
+
